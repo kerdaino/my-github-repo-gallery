acd97e049103f86eab6525c6fd737a5d1fe8cf65
kerdaino <kerdaino3@gmail.com>
2023-06-13 05:34:54 -0700

    second-commit

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@
 const overview = document.querySelector(".overview");
 // other variables
 const username = "kerdaino";
+const repoList = document.querySelector(".repo-list");
 
 
 const gith = async function () {
@@ -28,4 +29,23 @@ const displayGithInfo = function (users) {
         <p><strong>Number of public repos:</strong> ${users.public_repos}</p>
     </div>`;
     overview.append(div);
+    repo();
+};
+
+const repo = async function () {
+    const getRepo = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const returnGetRepo = await getRepo.json();
+    // console.log(returnGetRepo);
+    displayRepoList(returnGetRepo);
+};
+
+// repo();
+
+const displayRepoList = function (repos) {
+    for (const repo of repos) {
+        const repoListItem = document.createElement("li");
+        repoListItem.classList.add("repo");
+        repoListItem.innerHTML = `<h3>${repo.name}</h3>`;
+        repoList.append(repoListItem);
+    };
 };
